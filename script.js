bada41b8a6774582d75ef99bb0839020be507145
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-11-03 17:45:02 +0100

    event on typing

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ const tagsEl = document.getElementById("tags");
 const textArea = document.getElementById("textarea");
 
 textArea.focus();
+
+textArea.addEventListener("keyup", (e) => {
+  createTags(e.target.value);
+});
+
+function createTags(input) {}

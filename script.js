e9bcc7bc12da11e1667209dfe9f17ac1f3ad0aa6
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-11-03 18:01:30 +0100

    put input into array

--- script.js
+++ script.js
@@ -7,4 +7,10 @@ textArea.addEventListener("keyup", (e) => {
   createTags(e.target.value);
 });
 
-function createTags(input) {}
+function createTags(input) {
+  const tags = input
+    .split(",")
+    .filter((tag) => tag.trim() !== "")
+    .map((tag) => tag.trim());
+  console.log(tags);
+}
